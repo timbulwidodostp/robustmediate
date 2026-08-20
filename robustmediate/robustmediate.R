ee684242a8103c37bea7510d66346e62991058eb
Timbul Widodo, S.TP <160803827+timbulwidodostp@users.noreply.github.com>
2026-08-20 08:56:57 +0700

    robustmediate (RobustMediate) With (In) R Software

--- robustmediate/robustmediate.R
+++ robustmediate/robustmediate.R
@@ -8,7 +8,6 @@ library("RobustMediate")
 robustmediate = read.csv("https://raw.githubusercontent.com/timbulwidodostp/robustmediate/main/robustmediate/robustmediate.csv",sep = ";")
 robustmediate <- robustmediate(X ~ Z1 + Z2 + Z3, M ~ X + Z1 + Z2 + Z3, Y ~ X + M + Z1 + Z2 + Z3, data = robustmediate, R = 50)
 robustmediate
-diagnose(robustmediate)
 # Causal Mediation Analysis with Diagnostics and Sensitivity Analysis Use robustmediate (RobustMediate) With (In) R Software
 # Olah Data Semarang
 # WhatsApp : +6285227746673
